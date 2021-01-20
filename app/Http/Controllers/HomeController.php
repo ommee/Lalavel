cceac15f0cb4084035ef7fef50c12fdee1ba2a88
noppharat <615021000232@mail.rmutk.ac.th>
2021-01-20 10:34:15 +0700

    text

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class HomeController extends Controller {
+    
+    function Morning($id) {
+        return "<h1>Good morning {$id}</h1>";
+    }
+}
